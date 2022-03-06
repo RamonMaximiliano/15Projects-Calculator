4d5d07fd4ade10805ff1f58386c1268ce4b15c78
Ramon Maximiliano Wilbert <ramon_max1986@hotmail.com>
2022-03-06 17:01:27 -0300

    Starting to calc

--- script.js
+++ script.js
@@ -1,4 +1,23 @@
 
+let mathCalc = document.querySelectorAll(".primitive");
+for (let number of mathCalc){
+    document.addEventListener("click", showTheMath);
+}
+
+
+function showTheMath(numberCalc){
+    let resultText = document.querySelector(".calculating").innerHTML
+    document.querySelector(".calculating").innerHTML = `${resultText}${numberCalc.target.id}`
+    console.log(numberCalc.target.id)
+}
+
 function calc(){
-    console.log("Hello world!")
-};
+    let stringToCalc = document.querySelector(".calculating").innerHTML
+    console.log(stringToCalc)    
+    document.querySelector(".result").innerHTML = eval(stringToCalc)
+}
+
+function cleanMath(){
+    document.querySelector(".result").innerHTML = ''
+    document.querySelector(".calculating").innerHTML = ''
+}
